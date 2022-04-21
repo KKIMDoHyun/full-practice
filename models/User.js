d706fb258a0f0a7decbb82a453c9bee63c5a561c
dohyun_kim <dohyun_kim4@tmax.co.kr>
2022-04-21 18:24:47 +0900

    [feat] bcrypt 설치

--- models/User.js
+++ models/User.js
@@ -1,5 +1,7 @@
 const mongoose = require("mongoose");
 
+const bcrypt = require("bcrypt");
+
 const userSchema = mongoose.Schema({
   name: {
     type: String,
@@ -31,6 +33,11 @@ const userSchema = mongoose.Schema({
   tokenExp: {
     type: Number,
   },
+});
+
+// 유저 정보를 저장하기 전에 한다. 이 실행이 다끝나면 유저 정보를 시작한다.
+userSchema.pre("save", (next) => {
+  // 비밀번호를 암호화 시킨다.
 });
 
 const User = mongoose.model("User", userSchema);
